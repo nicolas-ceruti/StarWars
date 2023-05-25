bc5004e017af29cf2942390fbf24fa4ada3fc56a
Nicolas Andrei Ceruti <79486020+nicolas-ceruti@users.noreply.github.com>
2023-05-24 21:36:04 -0300

    css home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Container, Card } from './style';
+import { Container, Card, Filter, PagePanel, Button, Title } from './style';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { TbCircleArrowLeft, TbCircleArrowRight } from 'react-icons/tb'
 
 
 
@@ -9,7 +10,8 @@ function App() {
 
   const navigate = useNavigate()
   const [people, setPeople] = useState([]);
-  const page = 1;
+  const [allPeople, setAllPeople] = useState([]);
+  const [page, setPage] = useState(1);
   const baseUrl = 'https://swapi.dev/api/people/?page=';
 
   useEffect(() => {
@@ -25,15 +27,33 @@ function App() {
     }
   }, [page]);
 
+
+  useEffect(() => {
+      axios.get(`${baseUrl + page}`)
+        .then(response => {
+          setAllPeople([...allPeople, response.data.results]);
+          localStorage.removeItem(`peoples`)
+          localStorage.setItem(`peoples`, JSON.stringify(allPeople))
+        })
+        .catch(error => { console.error(error); });
+    
+  }, [page]);
+
   return (
     <Container>
+      
       {people.map((obj, i) => (
-        <Card key={i} onClick={(e) => navigate(`/profile/${i}`)}>
+        <Card key={i} onClick={(e) => navigate(`/profile/${page * 10 + 1 + i - 10 }`)}>
           <div>
-            <h2>{obj.name}</h2>
+            <Title>{obj.name}</Title>
           </div>
         </Card>
       ))}
+      <Filter>
+        <Button onClick={(e) => page - 1 == 0 ? setPage(page) : setPage(page - 1)}><TbCircleArrowLeft/></Button>
+        <PagePanel>{page}</PagePanel>
+        <Button onClick={(e) => page + 1 == 10 ? setPage(page) : setPage(page + 1)}><TbCircleArrowRight/></Button>
+      </Filter>
     </Container>
   );
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const Container = styled.div`
     padding: 10px;
     margin: 200px 200px 200px 200px;
-    height: 50vh;
+    min-height: 50vh;
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
@@ -22,16 +22,24 @@ export const Container = styled.div`
 `;
 
 export const Card = styled.div`
+    font-family: 'Star Jedi', arial;;
+    letter-spacing: 2px;
     padding: 10px;
     margin: 20px;
     height: 130px;
     min-width: 330px;
     display: flex;  
-    border-radius: 10px;
-    background: rgba(255,255,255,0.6);
-    -webkit-backdrop-filter: blur(28px);
-    backdrop-filter: blur(28px);
-    border: 1px solid rgba(255,255,255,0.3);
+    border-radius: 5px;
+    background: #1d1e1f;
+    cursor: pointer;
+    padding: 15px;
+
+    -webkit-box-shadow: 16px 19.5px 22px -17.5px #5e5e5e;
+    -moz-box-shadow: 16px 19.5px 22px -17.5px #5e5e5e;
+    box-shadow: 16px 19.5px 22px -17.5px #5e5e5e;
+
+
+    color: white;
 
     @media (max-width: 500px){
         min-width: 230px;
@@ -39,3 +47,72 @@ export const Card = styled.div`
     }
 `;
 
+
+export const Title = styled.div`
+    font-size: 20px;
+    font-family: 'Star Wars',sans-serif;
+    letter-spacing: 2px;
+    min-width: 330px;
+    display: flex;
+    border-radius: 5px;
+    color: white;
+    display: flex;
+`;
+
+
+export const Filter = styled.div`
+    padding: 10px;
+    margin: 15px 200px 10px 200px;
+    width: 100%;
+
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    @media (max-width: 1400px){
+        margin: 0px 80px 10px 80px;
+    }
+
+    @media (max-width: 1100px){
+        margin: 0px 20px 10px 20px;
+    }
+
+    @media (max-width: 500px){
+        margin: 0px 0px 10px  0px;
+    }
+`;
+
+export const PagePanel = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    background: transparent;
+    border-radius: 50% 50% 50% 50% / 50% 50% 50% 50%;
+    width: 50px;
+    height: 50px;
+    z-index: -1;
+    color: white;
+    font-size: 20px;
+    -webkit-box-shadow: 1px 1px 147px 13px rgba(89,0,76,1);
+    -moz-box-shadow: 1px 1px 147px 13px rgba(89,0,76,1);
+    box-shadow: 1px 1px 147px 13px rgba(89,0,76,1); 
+`;
+
+export const Button = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    cursor: pointer;
+    border-radius: 50% 50% 50% 50% / 50% 50% 50% 50%;
+    width: 50px;
+    height: 50px;
+    color: #9b9b9b;
+    font-size: 20px;
+    border: none;
+    margin: 0px 30px;
+    
+`;
